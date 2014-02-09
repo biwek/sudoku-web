27897cbc1a465760dab425ae073353ecfcbcc635
biwek <biweks@gmail.com>
2014-02-09 15:08:32 +0000

    added helpers to sudoku

--- sudoku.rb
+++ sudoku.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require_relative './lib/sudoku'
 require_relative './lib/cell'
+require_relative './helpers/application'
 configure :production do 
 	require 'newrelic_rpm'
 end
@@ -8,14 +9,25 @@ end
 enable :sessions
 
 def random_sudoku 
-	seed = (1..9).to_a.shuffle + Array.new(72, 0)
+	seed = (1..9).to_a.shuffle + Array.new(81-9, 0)
 	sudoku = Sudoku.new(seed.join)
 	sudoku.solve!
 	sudoku.to_s.chars
 end
 
 def puzzle(sudoku)
-	sudoku
+	new_sudoku = sudoku.dup
+  (0..81).to_a.sample(25).each { |i| new_sudoku[i] =" "}
+  new_sudoku
+end
+
+get '/' do 
+	prepare_to_check_solution
+	generate_new_puzzle_if_necessary
+	@current_solution = session[:current_solution] || session[:puzzle]
+	@solution = session[:solution]
+	@puzzle = session[:puzzle]
+	erb :index
 end
 
 def box_order_to_row_order(cells)
@@ -25,12 +37,13 @@ def box_order_to_row_order(cells)
 		three_boxes = boxes[first_box_index, 3] 
 		three_rows_of_three = three_boxes.map do |box|
 			row_number_in_a_box = i % 3
-			first_cell_in_the_row = row_number_in_a_box * 3
-			box[first_cell_in_the_row, 3]
+			first_cell_in_the_row_index = row_number_in_a_box * 3
+			box[first_cell_in_the_row_index, 3]
 		end
 		memo += three_rows_of_three.flatten
 	}
 end
+
 
 def generate_new_puzzle_if_necessary
 	return if session[:current_solution]
@@ -45,40 +58,9 @@ def prepare_to_check_solution
 	session[:check_solution] = nil
 end
 
-helpers do 
-	def colour_class(solution_to_check, puzzle_value, current_solution_value, solution_value)
-		must_be_guessed = puzzle_value == 0
-		tried_to_guess = current_solution_value.to_i != 0
-		guessed_incorrectly = current_solution_value != solution_value
-
-		if solution_to_check && must_be_guessed && tried_to_guess && guessed_incorrectly
-			'incorrect'
-		elsif !must_be_guessed
-			'value-provided'
-		end
-	end
-end
-
-helpers do 
-	def cell_value(value) 
-		value.to_i == 0 ? '' : value
-	end
-end
-
-
-
-
-
-get '/' do 
-	prepare_to_check_solution
-	generate_new_puzzle_if_necessary
-	@current_solution = session[:current_solution] || session[:puzzle]
-	@solution = session[:solution]
-	@puzzle = session[:puzzle]
-	erb :index
-end
 
 get '/solution' do 
+	puts session
 	@current_solution = session[:solution]
 	erb :index
 end
